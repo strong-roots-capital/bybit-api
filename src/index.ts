9231807d865a39d85b88ce32e187defae7cf81d8
Eric Crosson <eric.s.crosson@utexas.edu>
2021-09-19 14:08:54 -0500

    feat: export BybitTimeframe and BybitTradepair

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export * from './websocket-public'
 export * from './rest'
+export {
+  BybitTimeframe,
+  BybitTimeframeFromString,
+  Parser as BybitTimeframeParser,
+} from './codecs/BybitTimeframe'
+export { BybitTradepair, Parser as BybitTradepairParser } from './codecs/BybitTradepair'
